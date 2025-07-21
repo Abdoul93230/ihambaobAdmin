7f7a80b22df376d70a072fa953544f183c2c09c7
Abdoul_Razak <abdoulrazak9323@gmail.com>
2025-07-21 07:03:17 -0700

    Abdoul Razak

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ function App() {
   return (
     // <Provider store={store}>
     <div className="App">
-      <h1>Admin Admin</h1>
+      <h1>Admin Admin2</h1>
       <BrowserRouter>
         {verificationComplete ? (
           <Routes>
